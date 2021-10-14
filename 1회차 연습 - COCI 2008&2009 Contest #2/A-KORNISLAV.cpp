7fcdfa7b26f752b40cccafe40c4215a52bba4a95
Dohoon Kim <70815390+hgmhc@users.noreply.github.com>
2021-10-14 15:48:25 +0900

    Update A-KORNISLAV.cpp

--- 1회차 연습 - COCI 2008&2009 Contest #2/A-KORNISLAV.cpp
+++ 1회차 연습 - COCI 2008&2009 Contest #2/A-KORNISLAV.cpp
@@ -1,1 +1,16 @@
+#include <bits/stdc++.h>
+#define REP(i,a,b) for (auto i = (a); i <= (b); ++i)
+#define debug(x) cout << #x << " is " << x << el
+#define el '\n'
+using namespace std; using ll = long long;
 
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    vector<int> v;
+    REP(i,1,4) {
+        int x; cin >> x;
+        v.push_back(x);
+    }
+    sort(v.begin(), v.end());
+    cout << v[0]*v[2];
+}
